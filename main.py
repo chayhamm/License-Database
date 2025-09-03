25e365f3c9f99259cec2f274f113c2669bf32bae
Chay <chayhammond@gmail.com>
2025-09-03 19:55:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import asyncio
 
 async def Main():
-    pass
+    while True:
+        removeOrAdd = input("Type A to add | Type R to remove: ").lower()
+        if removeOrAdd == "a":
+            #link the add.py here
+            continue
+        if removeOrAdd == "r":
+            #link the remove.py here
+            continue
+        print("Invalid answer!")
+        continue
 
 asyncio.run(Main())
