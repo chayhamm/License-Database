35f1e53ece1e7ba1cddea9c99f818a6c87e345d8
Chay <chayhammond@gmail.com>
2025-09-04 21:05:07 +0100

    Update sendwebhook.py

--- sendwebhook.py
+++ sendwebhook.py
@@ -6,16 +6,19 @@ url = "https://ptb.discord.com/api/webhooks/1413243029589987379/3V6bLpfY7z4VyVwg
 def csendWebhook(fullKey, action): # add, remove
     data = {
         "content": "<@924269659774156822>",
-        "embeds": {
+        "embeds": [ 
+        {
             "title": "New Action",
             "description": action,
             "fields": [
                 {
                 "name": f'`{fullKey}`',
-                "value": datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+                "value": datetime.now().strftime("%d/%m/%Y, %H:%M:%S"),
+                "inline": False
                 }
             ]
         }
+        ]
     }
     response = requests.post(url, json = data)
     if response.status_code == 204:
