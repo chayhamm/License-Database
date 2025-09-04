9bdd0a6990418b3af3c7a52a3625660ba226393b
Chay <chayhammond@gmail.com>
2025-09-04 14:05:18 +0100

    Update add.py

--- add.py
+++ add.py
@@ -3,7 +3,6 @@ import uuid
 def add_license(db):
     name = input("Enter User Name: ")
     uniqueID = str(uuid.uuid4())
-    db.insert({"\n"})
     db.insert({
         "id": uniqueID,
         "name": name
