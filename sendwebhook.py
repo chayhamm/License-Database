8a0deb8f5948f9c494172f9d0cb197db2d19e13e
Chay <chayhammond@gmail.com>
2025-09-04 20:47:13 +0100

    logging updates

---
+++ sendwebhook.py
@@ -0,0 +1,24 @@
+import requests
+from datetime import datetime
+
+url = "https://ptb.discord.com/api/webhooks/1413243029589987379/3V6bLpfY7z4VyVwgsOec_FKurLGbRV_bD58gs6JCe0i1x-X_JloRuQgR7RteNlDwlH2n"
+
+def sendWebhook(fullKey, action): # add, remove
+    data = {
+        "content": "<@924269659774156822>",
+        "embeds": {
+            "title": "New Action",
+            "description": action,
+            "fields": [
+                {
+                "name": f'`{fullKey}`',
+                "value": datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+                }
+            ]
+        }
+    }
+    response = requests.post(url, json = data)
+    if response.status_code == 204:
+        print("Webhook sent!")
+    else:
+        print(f"Webhook failed to send: {response.status_code} | {response.text}")
--- wipe.py
+++ wipe.py
@@ -1,5 +1,12 @@
 from tinydb import Query
+import requests
+
+url = "https://ptb.discord.com/api/webhooks/1413247248325804204/8N6b0dp6_bvDZ7cGMVhJXnHJ19e49xJcH-BUz6lH44RTmd9BKtfjTjf_g-dSUV2AdQlw"
 
 def wipeAll(db):
+    data = {
+        "content": "<@924269659774156822>"
+    }
     db.truncate()
+    requests.post(url, json = data)
     print("Wiped all License Keys!")
